6e76ea644b02b264f4fcb69658e8e96317f5d0c7
Matthias Berchtold <matthi.berchtold@gmail.com>
2022-06-26 12:08:10 +0200

    v1.0.0

---
+++ README.md
@@ -0,0 +1,121 @@
+<!-- PROJECT LOGO -->
+<br />
+<div align="center">
+  <h3 align="center">Hide Data In JPG</h3>
+
+  <p align="center">
+    Hide data safely in a JPG image.
+  </p>
+</div>
+
+<!-- TABLE OF CONTENTS -->
+<details>
+  <summary>Table of Contents</summary>
+  <ol>
+    <li>
+      <a href="#about-the-project">About The Project</a>
+      <ul>
+        <li><a href="#how-it-works">How It Works</a></li>
+        <li><a href="#built-with">Built With</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#getting-started">Getting Started</a>
+      <ul>
+        <li><a href="#prerequisites">Prerequisites</a></li>
+        <li><a href="#installation">Installation</a></li>
+      </ul>
+    </li>
+    <li><a href="#usage">Usage</a></li>
+  </ol>
+</details>
+
+<!-- ABOUT THE PROJECT -->
+
+## About The Project
+
+This project aims to use steganography to allow communication between two users to be hidden using JPG images.
+
+### How It Works
+
+Since a JPG file ends with the sequence `0xffd9`, hidden data can be inserted after it (in the example, the jpg delimiter is on line `00002280`).
+
+```sh
+> xxd hidden.jpg
+00000000: ffd8 ffe0 0010 4a46 4946 0001 0100 0001  ......JFIF......
+00000010: 0001 0000 ffe2 0228 4943 435f 5052 4f46  .......(ICC_PROF
+00000020: 494c 4500 0101 0000 0218 0000 0000 0430  ILE............0
+00000030: 0000 6d6e 7472 5247 4220 5859 5a20 0000  ..mntrRGB XYZ ..
+00000040: 0000 0000 0000 0000 0000 6163 7370 0000  ..........acsp..
+...
+00002260: 8808 8880 8888 0888 8088 8808 8880 8888  ................
+00002270: 0888 8088 8808 8880 8888 0888 8088 8808  ................
+00002280: 8883 ffd9 6741 4141 4141 4269 6a51 4d43  ....gAAAAABijQMC
+00002290: 5674 6f4e 766c 6c6a 4e6a 4256 4d64 5275  VtoNvlljNjBVMdRu
+000022a0: 6644 7972 6569 4c67 4b6d 366f 3879 6164  fDyreiLgKm6o8yad
+000022b0: 5847 3547 2d37 6b46 716a 2d53 4434 355a  XG5G-7kFqj-SD45Z
+000022c0: 7573 644b 5742 7a49 724f 4863 6b78 4a62  usdKWBzIrOHckxJb
+000022d0: 6f59 414e 6c4a 617a 6173 4135 5778 7547  oYANlJazasA5WxuG
+000022e0: 396a 3561 7a51 3d3d                      9j5azQ==
+```
+
+To improve security, data is encrypted using Fernet symmetric encryption.
+
+The symmetric key used by this algorithm can be shared using 2048-bit RSA asymmetric encryption, as shown in the example in the <a href="#usage">Usage</a> section.
+
+### Built With
+
+- [Python](https://www.python.org/)
+
+<!-- GETTING STARTED -->
+
+## Getting Started
+
+To get a local copy up and running follow these simple example steps.
+
+### Prerequisites
+
+- python 3
+- pip3
+
+### Installation
+
+1. Clone the repo
+   ```sh
+   git clone https://github.com/iammatthi/hide-data-jpg.git
+   ```
+2. Move into the newly created folder
+   ```sh
+   cd hide-data-jpg
+   ```
+3. Install python packages
+   ```sh
+   pip3 install -r requirements.txt
+   ```
+
+<!-- USAGE EXAMPLES -->
+
+## Usage
+
+```sh
+# image.jpg is an image on the disk
+
+# Client 1
+# Generate RSA 2048 bit key pair and generate an image with the public key hidden (pub.jpg)
+python3 app.py keys --image image.jpg --image-out pub.jpg
+
+# Client 2
+# Generate Fernet key, encrypt it with the public key received in the image (pub.jpg) and
+# generate image (sym.jpg) with this encrypted key
+python3 app.py keys --public-key-image pub.jpg --image image.jpg --image-out sym.jpg
+
+# Client 1
+# Extract Fernet key from image (sym.jpg) and save it to disk
+python3 app.py keys --symmetric-key-image sym.jpg
+# Create image with hidden data (hidden.jpg)
+python3 app.py hide --image image.jpg --image-out hidden.jpg --text "Hi! How are you?"
+
+# Client 2
+# Read hidden data from image (hidden.jpg)
+python3 app.py extract --image hidden.jpg
+```
---
+++ app.py
@@ -0,0 +1,174 @@
+import argparse
+from os import path
+
+import rsa
+from cryptography.fernet import Fernet
+
+jpg_delimiter = bytes.fromhex('FFD9')
+
+private_asymmetric_key_file = "private_asymmetric.key"
+public_asymmetric_key_file = "public_asymmetric.key"
+
+
+def hide(image, image_out, data):
+    # Read input image
+    with open(image, 'rb') as fin:
+        image_data = fin.read()
+        offset = image_data.index(jpg_delimiter)
+
+    # Generate image with data
+    with open(image_out, 'wb+') as fout:
+        fout.write(image_data[:offset + len(jpg_delimiter)])
+        fout.write(data)
+
+
+def extract(image):
+    # Read data from image
+    with open(image, 'rb') as fin:
+        image_data = fin.read()
+        offset = image_data.index(jpg_delimiter)
+        fin.seek(offset + len(jpg_delimiter))
+        return fin.read()
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    action_subparser = parser.add_subparsers(dest='action', required=True)
+
+    hide_action = action_subparser.add_parser('hide')
+    hide_action.add_argument('-k', '--symmetric-key',
+                             type=str, default="symmetric.key")
+    hide_action.add_argument('-i', '--image', type=str, required=True)
+    hide_action.add_argument('-o', '--image-out', type=str, required=True)
+    encryption_data = hide_action.add_mutually_exclusive_group(required=True)
+    encryption_data.add_argument('--file', type=str)
+    encryption_data.add_argument('--text', type=str)
+
+    extract_action = action_subparser.add_parser('extract')
+    extract_action.add_argument('-k', '--symmetric-key',
+                                type=str, default="symmetric.key")
+    extract_action.add_argument('-i', '--image', type=str, required=True)
+    extract_action.add_argument('--file', type=str)
+
+    keys = action_subparser.add_parser('keys')
+    keys.add_argument('-k', '--symmetric-key',
+                      type=str, default="symmetric.key")
+    # FIXME: --image and --image-out should be required when --symmetric-key-image is not specified
+    keys.add_argument('-i', '--image', type=str)
+    keys.add_argument('-o', '--image-out', type=str)
+    key_image = keys.add_mutually_exclusive_group()
+    key_image.add_argument('-p', '--public-key-image', type=str)
+    key_image.add_argument('-s', '--symmetric-key-image', type=str)
+
+    args = parser.parse_args()
+    if args.action == 'keys':
+        if args.public_key_image:
+            # Public key image received -> Generate symmetric key image
+
+            # Read public key from image
+            public_key = rsa.PublicKey.load_pkcs1(
+                extract(args.public_key_image))
+
+            if path.exists(args.symmetric_key):
+                # Read symmetric key from file
+                with open(args.symmetric_key, "rb") as fin:
+                    key = fin.read()
+            else:
+                # Generate symmetric key
+                key = Fernet.generate_key()
+                # Save symmetric key
+                with open(args.symmetric_key, "wb+") as fout:
+                    fout.write(key)
+
+            data = rsa.encrypt(key, public_key)
+            hide(args.image, args.image_out, data)
+            print("Symmetric key image generated:", args.image_out)
+
+        elif args.symmetric_key_image:
+            # Symmetric key image received -> Save it to file
+
+            if path.exists(private_asymmetric_key_file) and path.exists(public_asymmetric_key_file):
+                # Read public key from file
+                with open(private_asymmetric_key_file, "rb") as fin:
+                    private_key = rsa.PrivateKey.load_pkcs1(fin.read())
+            else:
+                print("Asymmetric keys not found")
+                exit(1)
+
+            # Read symmetric key from image
+            data = extract(args.symmetric_key_image)
+            key = rsa.decrypt(data, private_key)
+            # Save symmetric key
+            with open(args.symmetric_key, "wb+") as fout:
+                fout.write(key)
+
+            print("Symmetric key saved:", args.symmetric_key)
+
+        else:
+            # Generate public key image
+
+            if path.exists(private_asymmetric_key_file) and path.exists(public_asymmetric_key_file):
+                # Read public key from file
+                with open(public_asymmetric_key_file, "rb") as fin:
+                    public_key = rsa.PublicKey.load_pkcs1(fin.read())
+            else:
+                # Generate asymmetric keys
+                public_key, private_key = rsa.newkeys(2048)
+                # Save private key
+                with open(public_asymmetric_key_file, "wb+") as fin:
+                    fin.write(public_key.save_pkcs1())
+                    print(public_asymmetric_key_file + " generated")
+                # Save public key
+                with open(private_asymmetric_key_file, "wb+") as fin:
+                    fin.write(private_key.save_pkcs1())
+                    print(private_asymmetric_key_file + " generated")
+
+            data = public_key.save_pkcs1()
+            hide(args.image, args.image_out, data)
+            print("Public key image generated:", args.image_out)
+
+    elif args.action == 'hide':
+        if path.exists(args.symmetric_key):
+            # Read symmetric key from file
+            with open(args.symmetric_key, "rb") as fin:
+                key = fin.read()
+        else:
+            print("Symmetric key not found")
+            exit(1)
+
+        if args.file:
+            with open(args.file, 'rb') as f:
+                data = f.read()
+        elif args.text:
+            data = args.text.encode()
+        else:
+            raise Exception("Encrytion data not found")
+
+        f = Fernet(key)
+        encrypted_data = f.encrypt(data)
+        hide(args.image, args.image_out, encrypted_data)
+        print("Image generated:", args.image_out)
+
+    elif args.action == 'extract':
+        if path.exists(args.symmetric_key):
+            # Read symmetric key from file
+            with open(args.symmetric_key, "rb") as fin:
+                key = fin.read()
+        else:
+            print("Symmetric key not found")
+            exit(1)
+
+        encrypted_data = extract(args.image)
+        f = Fernet(key)
+        data = f.decrypt(encrypted_data)
+
+        if args.file:
+            with open(args.file, 'wb') as fout:
+                fout.write(data)
+                print("File extracted:", args.file)
+        else:
+            print("Hidden text: " + data.decode('utf-8'))
+
+
+if __name__ == '__main__':
+    main()
